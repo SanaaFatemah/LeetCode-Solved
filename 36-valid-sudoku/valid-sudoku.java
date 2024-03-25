e2215cb03e74c072cd4c6cc263b3f7eb436e3f1a
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-03-25 12:40:26 -0700

    Time: 2 ms (78.87%) | Memory: 43.9 MB (93.09%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i<9; i++){
+            //The calculation i / 3 determines which row block the current row belongs to, 
+            //and then multiplying it by 3 gives the starting row index of that block.
+            int RowIndex = 3*(i/3);
+            //The calculation i % 3 determines the column block within which the current row belongs, 
+            //and then multiplying it by 3 gives the starting column index of that block.
+            int ColIndex = 3*(i%3);
+        HashSet<Character> rows = new HashSet<Character>();
+        HashSet<Character> columns = new HashSet<Character>();
+        HashSet<Character> cube = new HashSet<Character>();
+        for (int j = 0; j < 9;j++){
+            if(board[i][j]!='.' && !rows.add(board[i][j]))
+                return false;
+            if(board[j][i]!='.' && !columns.add(board[j][i]))
+                return false;
+            // This expression calculates the row index of the current cell within the 3x3 subgrid
+            //RowIndex = gives the row block of the cell
+            //j/3 determines which row within the subgrid the current cell belongs to (it ranges from 0 to 2).
+
+            //This expression calculates the column index of the current cell within the 3x3 subgrid.
+            //j%3 determines which column within the subgrid the current cell belongs to (it ranges from 0 to 2).
+            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+                return false;
+        }
+    }
+    return true;
+    }
+}
