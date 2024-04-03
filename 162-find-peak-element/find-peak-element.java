fd8f0ac0c0603cd6cc19129b66ce8375617b9ced
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-04-03 12:48:24 -0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (79.80%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -9,18 +9,23 @@ class Solution {
         while (low<=high)
         {
             int mid = low + (high-low)/2;
-
+            //This is an edge case for eg [1,2,3] => inx 2 will be returned. 
+            //since 2 < 3 && mid == 3-1 => 2
+            //this goes for the other way around as well
+            //[5,2,3]
             if ((mid == 0 || num[mid-1]< num[mid]) && (mid == num.length-1 || num[mid+1] < num[mid]))
             return mid;
 
             //important : what if mid is already on 0 then array index out of bounds will happen. hence we put a check of if mid>0
+            //this is to eliminate the sorted part of the array
+            //if the previous element is greater than current element, which means the right side of the array is sorted and we skip it
             else if (mid > 0 && num[mid-1] > num[mid])
             {
                 high = mid -1;
 
             }
 
-            else
+            else if (mid < num.length && num[mid] < num[mid+1])
             {
                 low = mid + 1;
             }
