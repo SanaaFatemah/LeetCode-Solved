0e845e3f2413aaa9450b2460d8789ed05fc70c0d
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-05-29 21:37:00 -0700

    Time: 191 ms (23.43%) | Memory: 41.6 MB (31.22%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -8,8 +8,8 @@ class Solution {
         if (board == null)
             return false;
         dirs = new int[][] { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
-        int m = board.length;
-        int n = board[0].length;
+        m = board.length;
+        n = board[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (backtrack(board, word, 0, i, j))
@@ -23,8 +23,9 @@ class Solution {
         // base
         if (idx == word.length())
             return true;
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0||board[i][j] == '#') return false;
         
+        if (i < 0 || j < 0 || i == m || j == n || board[i][j] == '#')
+            return false;
         // logic
         if (board[i][j] == word.charAt(idx)) {
 
