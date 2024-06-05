41742ee65b535cd968ad2e1e5aa7db87d4df25fa
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-04 22:20:32 -0700

    Time: 35 ms (33.04%) | Memory: 64.5 MB (8.70%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -3,16 +3,22 @@ class Solution {
         int n = nums.length;
         int [] res = new int[n-k+1];
         int ri =0;
+        //using a doubly linked list to push and pop from either ends
         Deque<Integer> queue = new ArrayDeque<>();
         for(int i =0;i<n;i++)
         {
+            //if an element that is out of bounds of tht given window size is present, remove it
             if(!queue.isEmpty() && queue.peek() == i-k) queue.poll();
 
+            //checking if the values of the queue are in decreaing order
             while(!queue.isEmpty() && nums[queue.peekLast()] < nums[i])
             {
                 queue.pollLast();
             }
+            //we add the index of the element in the queue
             queue.offer(i);
+
+            //adding the maximum in the window into the res array
             if(i>=k-1)
             {
                 res[ri++] = nums[queue.peek()];
