aca88e273a0bafaae61f1292940d2da1fe41aef3
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-06 08:20:49 -0700

    Time: 0 ms (100.00%) | Memory: 45.6 MB (37.74%) - LeetSync

--- 280-wiggle-sort/wiggle-sort.java
+++ 280-wiggle-sort/wiggle-sort.java
@@ -1,6 +1,7 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-
+        //Observing a pattern in the final output where the odd indexed numbers were greater than the even indexed numbers.
+        //using that observation, we will compare the 2 and initiate a swap if the conditions are satisfied
         for(int i=0; i<nums.length-1;i++)
         {
             if((i%2==0 && nums[i] > nums[i+1]) ||(i%2==1 && nums[i] < nums[i+1]))
