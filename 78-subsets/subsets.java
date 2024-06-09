1e4dddb204c67f241803cbbe23c805bd24ddced0
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-08 17:38:56 -0700

    Time: 207 ms (52.62%) | Memory: 45.6 MB (7.55%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        helper(nums, 0, res, new ArrayList<Integer>());
-
-        return res;
-
+        List<List<Integer>> result=new ArrayList<>();
+        helper(result,new ArrayList<>(),nums,0);
+        return result;
     }
-
-    public void helper(int[] nums, int start, List<List<Integer>> res, ArrayList<Integer> tmp) {
-        // base
-        res.add(new ArrayList<>(tmp));
-        // recursion
-        for (int i = start; i < nums.length; i++) {
-
-            tmp.add(nums[i]);
-
-            helper(nums, i + 1, res, tmp);
-
-            // backytack
-            tmp.remove(tmp.size() - 1);
+    
+    private void helper(List<List<Integer>> result, List<Integer> output,int[] nums,int start)
+    {
+        if(start==nums.length){
+             result.add(new ArrayList<>(output));
+             //System.out.println("res "+result+" out "+output+" start "+start);
+            return;
         }
-
+            output.add(nums[start]);
+            helper(result,output,nums,start+1);
+            System.out.println("res "+result+" out "+output+" start "+start);
+            output.remove(output.size()-1);
+            helper(result,output,nums,start+1);
+  
     }
-
 }
