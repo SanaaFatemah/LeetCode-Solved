dae246ef1c1204847b3ab80a31ddcbe7ce4a144b
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-11 20:12:49 -0700

    Time: 6 ms (46.84%) | Memory: 45.7 MB (17.16%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length == 0) return true;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[] inDeg = new int[numCourses];
+
+        for (int[] pre : prerequisites) {
+            inDeg[pre[0]]++;
+            if (!map.containsKey(pre[1])) {
+                map.put(pre[1], new ArrayList<>());
+            }
+            map.get(pre[1]).add(pre[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDeg[i] == 0) {
+                queue.add(i);
+                count++;
+            }
+
+        }
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            List<Integer> children = map.get(curr);
+            //children = map.get(curr);
+            if (children != null) {
+                for (int child : children) {
+                    inDeg[child]--;
+                    if (inDeg[child] == 0) {
+                        queue.add(child);
+                        count++;
+                        if(count == numCourses) return true;
+                    }
+
+                }
+            }
+
+
+        }
+        return false;
+
+    }
+}
