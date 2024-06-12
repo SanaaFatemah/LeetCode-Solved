fb7d3d2ff96014916f1709b8fa9bf21f93ee5125
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-11 20:17:42 -0700

    Time: 7 ms (33.79%) | Memory: 45.7 MB (21.28%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,9 +1,15 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(prerequisites.length == 0) return true;
+        //Creating a hash map that will serve as an adjacency list
+        //this map shows which node is dependent on which node.
+        //independent : <List of dependent nodes>
         HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        //This array is created to show that which node has how many dependencies, the count of it
         int[] inDeg = new int[numCourses];
 
+        //creating the adjacency list
         for (int[] pre : prerequisites) {
             inDeg[pre[0]]++;
             if (!map.containsKey(pre[1])) {
@@ -12,6 +18,8 @@ class Solution {
             map.get(pre[1]).add(pre[0]);
         }
 
+        //This queue is used to process all the independent nodes first,
+        // it will start the node that has an indegree count of 0
         Queue<Integer> queue = new LinkedList<>();
         int count = 0;
 
@@ -22,6 +30,10 @@ class Solution {
             }
 
         }
+        //using the value in the queue to search the adjacency list and looking for all the nodes dependent on it.
+        //this is bascially a way of completeting the independent course which is a pre requisite for the other courses
+        //once we start with the independent node we look for that key in map and get a list of the courses that are dependednt on it
+        //we reduce their indegree value as now they are completed for that course in the queue
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             List<Integer> children = map.get(curr);
