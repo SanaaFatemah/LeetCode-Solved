b426667961a85e98658eeecee7d0c9d7474e1fda
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-06-12 14:50:21 -0700

    Time: 1 ms (99.98%) | Memory: 42 MB (77.57%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -7,6 +7,8 @@ class Solution {
         int fresh = 0;
         int[][] dirs = new int[][] { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 
+        //Step 1 : get the indices of all the rotten oranges and count of the fresh oranges
+        //Add the address of the rotten ones in the queue
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
@@ -22,17 +24,23 @@ class Solution {
 
         int time = 0;
 
+        //Step 2: Using BFS process each level of rotten oranges
         while (!q.isEmpty()) {
             int size = q.size();
+            //Making sure to process each batch together
             for (int i = 0; i < size; i++) {
+                //Extracting one rotten orange
                 int[] curr = q.poll();
+                //Exploring its neighbors
                 for (int[] dir : dirs) {
                     int nr = dir[0] + curr[0];
                     int nc = dir[1] + curr[1];
-
+                    //Checking index out of bounds and if the neighbor is a fresh orange
                     if (nr >= 0 && nc >= 0 && nc < n && nr < m && grid[nr][nc] == 1) {
                         q.add(new int[] { nr, nc });
+                        //Mutating the grid to mark it as rotten now
                         grid[nr][nc] = 2;
+                        //Decreasing the fresh count
                         fresh--;
                     }
                 }
