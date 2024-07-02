0a95f32786d3d47461f154a05129771bcbf1d587
SanaaFatemah <98856277+SanaaFatemah@users.noreply.github.com>
2024-07-02 08:38:02 -0700

    Time: 1 ms (94.60%) | Memory: 45.3 MB (40.32%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -2,6 +2,7 @@ class Solution {
     public int countBattleships(char[][] board) {
 	//count of our battleships
 	int n = 0;
+    int [][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
 
 	//iterate thru the board
 	for (int i = 0; i < board.length; i++) {
@@ -11,7 +12,7 @@ class Solution {
 				//increment count
 				n++;
 				//sink ship
-				DFS(board, i, j);
+				DFS(board, i, j, dirs);
 			}
 		}
 	}
@@ -21,28 +22,38 @@ class Solution {
 }
 
 //recursive helper method for DFS (sink the ships)
-private void DFS(char[][] board, int i, int j) {
+private void DFS(char[][] board, int i, int j, int [][] dirs) {
 	//sink current piece (change to dot)
 	board[i][j] = '.';
 
+    for(int dir[]:dirs)
+    {
+        int nr = i+dir[0];
+        int nc = j+dir[1];
+
+        if(nr >= 0 && nr < board.length && nc < board[0].length && nc >= 0 && board[nr][nc] == 'X')
+        {
+            DFS(board, nr, nc, dirs);
+        }
+    }
 	//up
-	if (i > 0 && board[i-1][j] == 'X') {
-		DFS(board, i-1, j);
-	}
+	// if (i > 0 && board[i-1][j] == 'X') {
+	// 	DFS(board, i-1, j);
+	// }
 
-	//down
-	if (i < board.length - 1 && board[i+1][j] == 'X') {
-		DFS(board, i+1, j);
-	}
+	// //down
+	// if (i < board.length - 1 && board[i+1][j] == 'X') {
+	// 	DFS(board, i+1, j);
+	// }
 
-	//left
-	if (j > 0 && board[i][j-1] == 'X') {
-		DFS(board, i, j-1);
-	}
+	// //left
+	// if (j > 0 && board[i][j-1] == 'X') {
+	// 	DFS(board, i, j-1);
+	// }
 
-	//right
-	if (j < board[i].length - 1 && board[i][j+1] == 'X') {
-		DFS(board, i, j+1);
-	}
+	// //right
+	// if (j < board[i].length - 1 && board[i][j+1] == 'X') {
+	// 	DFS(board, i, j+1);
+	// }
 }
 }
